8834289cd2251a65d42ebe14748f46c9e53bbc99
PetarQAjunior <femicpetar@gmail.com>
2023-04-29 18:43:16 +0200

    Added tests.txt files, first spec and started POMs

---
+++ cypress/support/POM/homepage.js
@@ -0,0 +1,10 @@
+const homePage = {
+    header: '#main-header',
+    categoriesBurgerMenu: '#entry_217833',
+    navigationBar: '#widget-navbar-217834',
+    mainBanner: '.active > a.d-block > .d-block',
+    promoSection1: '#entry_213241',
+    topTrendingCategories: '#entry_213248'
+}
+
+export { homePage }
